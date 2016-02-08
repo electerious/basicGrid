33cb655d30c534bc9fd081d90dcf9fd331cc9be4
Tobias Reich <tobias.reich.ich@gmail.com>
2016-02-08 11:09:27 +0100

    Updated gulpfile syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 'use strict'
 
-let name  = require('./package.json').moduleName,
-    gulp  = require('gulp'),
-    tasks = require('@electerious/basictasks')(gulp, name)
+let name  = require('./package.json').moduleName
+let gulp  = require('gulp')
+let tasks = require('@electerious/basictasks')(gulp, name)
 
 const styles = tasks.styles({
 	from : './src/styles/main.scss',
@@ -10,9 +10,9 @@ const styles = tasks.styles({
 })
 
 const watch = function() {
-	gulp.watch('./src/styles/**/*.scss', ['styles'])
+	gulp.watch('./src/styles/**/*.scss', [ 'styles' ])
 }
 
 gulp.task('styles', styles)
-gulp.task('default', ['styles'])
-gulp.task('watch', ['default'], watch)
+gulp.task('default', [ 'styles' ])
+gulp.task('watch', [ 'default' ], watch)
